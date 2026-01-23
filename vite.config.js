dbee49bf975359e5e3fc752d96f74a5f8a187fcf
Krazylemon <angeleduvelazquez@gmail.com>
2026-01-22 23:21:11 -0600

    holaaa

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   base:'./KrazyLemon',
-=======
-  base:'./KrazyLemon/',
->>>>>>> b1faa1b172162a6038772e965a7c63a5f15b08c3
   plugins: [
     react(),
     tailwindcss(),]
